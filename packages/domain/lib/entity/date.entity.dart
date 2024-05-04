75227594b53b404f459f9b79fab508433f9071d0
betterafter <koin12000@naver.com>
2024-05-05 03:18:08 +0900

    [feature] 프로젝트 구조 변경

--- packages/domain/lib/entity/date.entity.dart
+++ packages/domain/lib/entity/date.entity.dart
@@ -1,3 +1,5 @@
+import 'package:domain/extension.dart';
+import 'package:intl/intl.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'date.entity.g.dart';
@@ -16,7 +18,17 @@ class DateEntity {
   @JsonKey(name: 'holidayYn')
   bool? holidayYn;
 
-  DateEntity({this.dplId, this.date, this.enabled, this.holidayYn});
+  // 2024.05.05 (월)
+  String? fullDateString;
+
+  // 월화수목금토일
+  String? dayOfWeek;
+
+  DateEntity({this.dplId, this.date, this.enabled, this.holidayYn}) {
+    var week = date != null ? DateTime.parse(date!).weekday : null;
+    dayOfWeek = week != null ? weekDay[week] : null;
+    fullDateString = date?.replaceAll('-', '.');
+  }
 
   factory DateEntity.fromJson(Map<String, dynamic> json) =>
       _$DateEntityFromJson(json);
